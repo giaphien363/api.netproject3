4282f5d271d08ca7be83896696f44b09b2a0ff69
Ally Tran <h.anhtr15@gmail.com>
2022-02-24 19:41:30 +0700

    data: InsuranceCompany

---
+++ BaseProject/data/InsuranceCompany_202202241940.sql
@@ -0,0 +1,9 @@
+INSERT INTO api_dotnet.dbo.InsuranceCompany (Name,Address,Phone,Url,CreatedAt,UpdatedAt,IsDeleted) VALUES
+	 ('Bao Viet','8 Le Thai To, Hoan Kiem Dist., Hanoi','1900 55 88 99','https://www.baoviet.com.vn/','2022-02-23 19:51:36.587','2022-02-23 19:51:36.587',0),
+	 ('Generali','Trieste, Italy','1900 96 96 75','https://generali.vn/','2022-02-24 12:23:42.457','2022-02-24 12:23:42.457',0),
+	 ('Chubb Life','Floor 21, Sun Wah Building, 115 Nguyen Hue, Dist. 1, HCMC','(028) 3827 8123','https://www.chubb.com/','2022-02-24 12:29:18.03','2022-02-24 12:29:18.03',0),
+	 ('AIA','Floor 2, Bitexco Building, 2 Hai Trieu, Dist. 1, HCMC','(028) 3821 6400','https://www.aia.com.vn/','2022-02-24 12:29:51.307','2022-02-24 12:29:51.307',0),
+	 ('Dai-ichi Life','366 Nguyen Trong Tuyen, Tan Binh Dist., HCMC','(028) 3810 0888','https://www.dai-ichi-life.com.vn/','2022-02-24 12:32:07.627','2022-02-24 12:32:07.627',0),
+	 ('Manulife','Floor 2, Manulife Plaza, 75 Hoang Van Thai, Dist. 7, HCMC','1900 1776','https://www.manulife.com.vn/','2022-02-24 12:32:56.72','2022-02-24 12:32:56.72',0),
+	 ('Prudential','Floor 25, Sai Gon Trade Center, 37 Ton Duc Thang, HCMC','1800 1247','https://www.prudential.com.vn','2022-02-24 12:33:58.843','2022-02-24 12:33:58.843',0),
+	 ('MIC - Military Insurance Company','Floor 5-6, MB Building, 21 Cat Linh, Dong Da Dist., Hanoi','1900 55 88 91','https://mic.vn/','2022-02-24 12:35:00.233','2022-02-24 12:35:00.233',0);
